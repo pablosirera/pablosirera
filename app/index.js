9e7119d8d3fd8f6fcb0ee1af52d764a46ef90d1a
Pablo Sirera <psirera4@gmail.com>
2020-08-19 15:38:24 +0200

    add await to json parse

--- app/index.js
+++ app/index.js
@@ -19,7 +19,7 @@ const getLatestArticlesFromBlog = () =>
 const getPhotosFromInstagram = async () => {
   const response = await fetch(`https://www.instagram.com/pablodeveloper/`);
   const text = await response.text();
-  const json = JSON.parse(text.match(INSTAGRAM_REGEXP)[1]);
+  const json = await JSON.parse(text.match(INSTAGRAM_REGEXP)[1]);
   console.log(json)
   const edges = json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges.splice(
     0,
