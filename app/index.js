0afda880f8165f72b0634b7d70a8561cd55824e6
Pablo Sirera <psirera4@gmail.com>
2020-11-07 17:48:00 +0000

    fix: comment get ig photos

--- app/index.js
+++ app/index.js
@@ -7,28 +7,28 @@ import { PLACEHOLDERS, NUMBER_OF } from './constants.js';
 const parser = new Parser();
 
 const { YOUTUBE_API_KEY } = process.env;
-const INSTAGRAM_REGEXP = new RegExp(
-  /<script type="text\/javascript">window\._sharedData = (.*);<\/script>/
-);
+// const INSTAGRAM_REGEXP = new RegExp(
+//   /<script type="text\/javascript">window\._sharedData = (.*);<\/script>/
+// );
 
 const getLatestArticlesFromBlog = () =>
   parser
     .parseURL('https://pablosirera.com/feed.xml')
     .then((data) => data.items);
 
-const getPhotosFromInstagram = async () => {
-  const response = await fetch(`https://www.instagram.com/pablodeveloper/`);
-  const text = await response.text();
-  const json = await Promise.resolve(JSON.parse(text.match(INSTAGRAM_REGEXP)[1]));
-  const edges = json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges.splice(
-    0,
-    8
-  );
-  return edges.map(({ node }) => ({
-    permalink: `https://www.instagram.com/p/${node.shortcode}/`,
-    media_url: node.thumbnail_src,
-  }));
-};
+// const getPhotosFromInstagram = async () => {
+//   const response = await fetch(`https://www.instagram.com/pablodeveloper/`);
+//   const text = await response.text();
+//   const json = await Promise.resolve(JSON.parse(text.match(INSTAGRAM_REGEXP)[1]));
+//   const edges = json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges.splice(
+//     0,
+//     8
+//   );
+//   return edges.map(({ node }) => ({
+//     permalink: `https://www.instagram.com/p/${node.shortcode}/`,
+//     media_url: node.thumbnail_src,
+//   }));
+// };
 
 const getLatestYoutubeVideos = () =>
   fetch(
@@ -37,10 +37,10 @@ const getLatestYoutubeVideos = () =>
     .then((res) => res.json())
     .then((videos) => videos.items);
 
-const generateInstagramHTML = ({ media_url, permalink }) => `
-<a href='${permalink}' target='_blank'>
-  <img width='20%' src='${media_url}' alt='Instagram photo' />
-</a>`;
+// const generateInstagramHTML = ({ media_url, permalink }) => `
+// <a href='${permalink}' target='_blank'>
+//   <img width='20%' src='${media_url}' alt='Instagram photo' />
+// </a>`;
 
 const generateYoutubeHTML = ({ title, videoId }) => `
 <a href='https://youtu.be/${videoId}' target='_blank'>
@@ -48,11 +48,12 @@ const generateYoutubeHTML = ({ title, videoId }) => `
 </a>`;
 
 (async () => {
-  const [template, articles, videos, photos] = await Promise.all([
+  // const [template, articles, videos, photos] = await Promise.all([
+  const [template, articles, videos] = await Promise.all([
     fs.readFile('./app/README.md.tpl', { encoding: 'utf-8' }),
     getLatestArticlesFromBlog(),
     getLatestYoutubeVideos(),
-    getPhotosFromInstagram(),
+    // getPhotosFromInstagram(),
   ]);
 
   // create latest articles markdown
@@ -75,16 +76,16 @@ const generateYoutubeHTML = ({ title, videoId }) => `
   }
 
   // create latest photos from instagram
-  const latestInstagramPhotos = photos
-    .slice(0, NUMBER_OF.PHOTOS)
-    .map(generateInstagramHTML)
-    .join('');
+  // const latestInstagramPhotos = photos
+  //   .slice(0, NUMBER_OF.PHOTOS)
+  //   .map(generateInstagramHTML)
+  //   .join('');
 
   // replace all placeholders with info
   const newMarkdown = template
     .replace(PLACEHOLDERS.LATEST_ARTICLES, latestArticlesMarkdown)
-    .replace(PLACEHOLDERS.LATEST_YOUTUBE, latestYoutubeVideos)
-    .replace(PLACEHOLDERS.LATEST_INSTAGRAM, latestInstagramPhotos);
+    .replace(PLACEHOLDERS.LATEST_YOUTUBE, latestYoutubeVideos);
+    // .replace(PLACEHOLDERS.LATEST_INSTAGRAM, latestInstagramPhotos);
 
   await fs.writeFile('README.md', newMarkdown);
 })();
